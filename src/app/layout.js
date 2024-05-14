a8a9c7ac7ece33ae81f0f52ee4cbf00b687f8b67
Milici-Ilie <milicicristyan@yahoo.com>
2024-05-14 23:57:24 +0300

    ...

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div>
+        <div className="flex justify-between max-w-6xl mx-auto">
           <div>
             <Sidebar/>
           </div>
